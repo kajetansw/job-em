6579f17dbae4b508e6adc07f36791afc147e1fe1
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2025-05-21 11:48:01 +0200

    feat: included avery day from the range of the sent job applications

--- src/components/home/Dashboard/Dashboard.tsx
+++ src/components/home/Dashboard/Dashboard.tsx
@@ -1,6 +1,5 @@
 import { createSupabaseClient } from "@/utils/supabase/client";
-import { groupBy } from "lodash";
-import { DateTime } from "luxon";
+import { DateTime, Interval } from "luxon";
 import { JobChart } from "./JobChart";
 
 export default async function Dashboard() {
@@ -27,19 +26,38 @@ export default async function Dashboard() {
  */
 
 const toChartData = (data: { created_at: string }[]) => {
-  const normalizeDates = (d: (typeof data)[number]) => ({
-    ...d,
-    created_at:
-      DateTime.fromISO(d.created_at).startOf("day").toFormat("LLL dd, y") ?? "",
-  });
+  const formatDate = (date: DateTime) => date.toFormat("LLL dd, y");
 
-  const groupedByCreatedDate = groupBy(
-    data,
-    (d) => normalizeDates(d).created_at,
+  const dates = data.map((d) => DateTime.fromISO(d.created_at));
+
+  const range = Interval.fromDateTimes(
+    DateTime.min(...dates) ?? DateTime.now(),
+    DateTime.max(...dates)?.plus({ day: 1 }) ?? DateTime.now(),
+  )
+    .splitBy({ day: 1 })
+    .map((d) => d.start)
+    .filter(Boolean) as DateTime[];
+
+  const groupedByCreatedDate = range.reduce(
+    (acc, curr) => {
+      const toFormattedDate = (dateISO: string) =>
+        formatDate(DateTime.fromISO(dateISO));
+
+      return {
+        ...acc,
+        [formatDate(curr)]: data
+          .filter((d) => toFormattedDate(d.created_at) === formatDate(curr))
+          .map((d) => ({
+            ...d,
+            created_at: toFormattedDate(d.created_at),
+          })),
+      };
+    },
+    {} as Record<string, typeof data>,
   );
 
   return Object.entries(groupedByCreatedDate).map(([date, items]) => ({
     date,
-    sent: items?.length ?? 0,
+    sent: items.length,
   }));
 };
